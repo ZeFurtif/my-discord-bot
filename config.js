575664611d8254a781204238a476eb96b1c38a3e
Eliott <84229100+ZeFurtif@users.noreply.github.com>
2022-06-09 01:20:04 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    token: "ODkzODI1ODcxMzkwMDU2NDgw.GxlzjB.RMdta_FMZnFNWXugjy3sWzwhv67KWUrRjMSZBQ", // Replace this with you bot account's token. | https://discordjs.guide/preparations/setting-up-a-bot-application.html
+    token: "", // Replace this with you bot account's token. | https://discordjs.guide/preparations/setting-up-a-bot-application.html
     prefix: "**", // Replace this with the character that goes before all of your commands.
     statusMessage: "prefix : **", // Replace this with the status message you want for your bot. (The "Playing video games" under the bots name). More customization: https://discord.js.org/#/docs/main/stable/typedef/PresenceData (Just edit the ready.js if you are going to do this).
     dbltoken: undefined // You do not have to change this! Look at the bottom of the /events/ready.js file for more info on what this is for if you're interested in advertising your bot!
